73fa7661f222eed456b2ab87ba84a4238ad51212
HarryChaux98 <115327814+HarryChaux98@users.noreply.github.com>
2022-10-28 17:27:36 -0500

    Add files via upload

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,33 @@
+import logo from "./logoanny.jpg";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div>
+      <ul class="menu">
+        <p></p>
+        <img src={logo} width="115" height="115"></img>
+        <li>
+          <Link to="inicio">Inicio</Link>
+          <hr></hr>
+        </li>
+        <li>
+        <Link to="acercade">Acerca de Confecciones Anny</Link>
+          <hr></hr>
+        </li>
+        <li>
+            <Link to="location">Ubicacion</Link>
+            <hr></hr>
+          </li>
+        <li>
+          <Link to="camara">Camara</Link>
+          <hr></hr>
+        </li>
+        <li>
+          <Link to="RecordView">RecordView</Link>
+          <hr></hr>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Header;
